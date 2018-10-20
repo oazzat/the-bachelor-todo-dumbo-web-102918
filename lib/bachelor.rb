26a00c45045006c5abae0aaf8f628258548c0301
Omar A <oazzat19@gmail.com>
2018-10-20 22:05:52 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,5 +49,12 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data.each do |key,season|
+      season.each do |hash|
+        
+      if hash["occupation"] == occupation
+        return hash["name"]
+      end
+    end
+  end
 end
